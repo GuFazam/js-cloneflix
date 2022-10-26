841b710bcdec9191d78132953d9f72ed914ad62b
Gustavo H. Fazam <gufazam10@gmail.com>
2022-10-25 22:30:21 -0300

    CloneFlix
    cópia "simples" da netflix

---
+++ app.js
@@ -0,0 +1,20 @@
+let filme1 = 'imagens/filme01.jpg'
+let filme2 = 'imagens/filme02.jpg'
+let filme3 = 'imagens/filme03.jpg'
+let filme4 = 'imagens/filme04.jpg'
+let filme5 = 'imagens/filme05.jpg'
+let filme6 = 'imagens/filme06.jpg'
+document.querySelector(".container").innerHTML += "<img src='" + filme1 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme2 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme3 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme4 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme5 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme6 + "'> "
+
+var filmes = ['imagens/filme01.jpg', 'imagensfilme02.jpg', 'imagensfilme03.jpg', 'imagensfilme04.jpg', 'imagensfilme05.jpg', 'imagens/filme06.jpg'];
+document.querySelector(".container").innerHTML += "<img src='" + filme01 + "'> "[0];
+document.querySelector(".container").innerHTML += "<img src='" + filme02 + "'> "[1];
+document.querySelector(".container").innerHTML += "<img src='" + filme03 + "'> "[2];
+document.querySelector(".container").innerHTML += "<img src='" + filme04 + "'> "[3];
+document.querySelector(".container").innerHTML += "<img src='" + filme05 + "'> "[4];
+document.querySelector(".container").innerHTML += "<img src='" + filme06 + "'> "[5];
